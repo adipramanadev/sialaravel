8e3c3d28682ea6a0b207ef7a621ccc38e390e35d
adipramanadev <herryprasetyo.dev@gmail.com>
2023-09-13 11:14:00 +0700

    store

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 //rute jurusan
 Route::get('jurusan',[App\Http\Controllers\JurusanController::class, 'index'])->name('jurusan.index');
 Route::get('create-jurusan',[App\Http\Controllers\JurusanController::class, 'create'])->name('jurusan.create');
-
+Route::post('store-jurusan',[App\Http\Controllers\JurusanController::class, 'store'])->name('jurusan.store');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
